d38b8e50ac77911f7de83d500df8cfba6d672c9b
gitGurugu <146617196+gitGurugu@users.noreply.github.com>
2025-04-04 15:01:27 +0800

    Delete backend directory

--- backend/app/schemas/wechat.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-from pydantic import BaseModel
-
-class WechatCode2SessionResponse(BaseModel):
-    openid: str
-    session_key: str
-    unionid: Optional[str] = None
-    errcode: Optional[int] = None
-    errmsg: Optional[str] = None
